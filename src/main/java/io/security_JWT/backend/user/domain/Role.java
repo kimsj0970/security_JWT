ee4987aabda7c669ba9bef49eecc5e00e138d26a
kimsj0970 <kimsj0970@naver.com>
2025-05-30 17:25:29 +0900

    refact: user로 변경

---
+++ src/main/java/io/security_JWT/backend/user/domain/Role.java
@@ -0,0 +1,6 @@
+package io.security_JWT.backend.user.domain;
+
+public enum Role {
+    ADMIN, USER
+
+}
---
+++ src/main/java/io/security_JWT/backend/user/dto/LoginResponseDto.java
@@ -0,0 +1,5 @@
+package io.security_JWT.backend.user.dto;
+
+public record LoginResponseDto(String accessToken, String refreshToken) {
+
+}
